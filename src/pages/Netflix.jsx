2486dee1e7971d53b4c5087004d5b2d749a61121
Luu Vu <vu7178@gmail.com>
2023-07-16 14:06:00 +0700

    navbar

--- src/pages/Netflix.jsx
+++ src/pages/Netflix.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+import Navbar from '../components/Navbar';
 
 const Netflix = () => {
-    return <div>Netflix</div>;
+    const [isScrolled, setIsScrolled] = React.useState(false);
+    window.onscroll = () => {
+        setIsScrolled(window.pageYOffset === 0 ? false : true);
+        return () => (window.onscroll = null);
+    };
+    return (
+        <div>
+            <Navbar isScrolled={isScrolled} />
+        </div>
+    );
 };
-
+const Container = styled.div``;
 export default Netflix;
